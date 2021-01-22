0fe81001ed9f7c90fe9ea0f11272725328a9a8a5
victorgt2406 <76251172+victorgt2406@users.noreply.github.com>
2021-01-22 16:13:38 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,31 +1,9 @@
 /*
-	INDEX 
-	
-	Autor: Víctor Gutiérrez Tovar
-	Fecha 29/01/2021
+	index.js
+
+	Convinatoria.
+
+	AUTOR: Víctor Gutiérrez Tovar
+	FECHA: 12/01/2021
+	TÍTULO: Index.
 */
-
-function fondo(){
-	setInterval(carrusel_de_fotos,2000);
-}
-
-//contadores.
-var change=1;
-var c=1;
-
-function carrusel_de_fotos(){
-	//fotos del main
-	if(parseInt(change)>3){
-		change=1;
-		c=parseInt(c);
-		c++;
-		if(c>=12){
-			c=1;
-		}
-	}
-	
-	document.getElementById("img"+change).src="img/index_imagenes/videojuegos_order/"+c+".jpg";
-
-	change=parseInt(change);
-	change++;
-}
